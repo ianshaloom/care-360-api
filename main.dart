984079d54a3151599fa75975a332b0e473274302
ianshaloom <ianshaloom0@outlook.com>
2025-02-05 21:08:05 +0300

    Update Dart package versions and environment variable handling in main.dart

--- main.dart
+++ main.dart
@@ -5,6 +5,13 @@ import 'dart:io';
 import 'package:dart_firebase_admin/dart_firebase_admin.dart';
 import 'package:dart_frog/dart_frog.dart';
 
+// Project ID, Client ID, Client Email, and Private Key
+String? projectId = Platform.environment['PROJECT_ID'];
+String? clientId = Platform.environment['CLIENT_ID'];
+String? cientEmail = Platform.environment['CLIENT_EMAIL'];
+String? privateKey =
+    Platform.environment['PRIVATE_KEY']?.replaceAll(r'\n', '\n');
+
 // Global variable to store the Firebase Admin instance
 FirebaseAdminApp? firebaseAdmin;
 
@@ -15,9 +22,11 @@ Future<void> init(InternetAddress ip, int port) async {
 
   // Initialize Firebase Admin SDK
   firebaseAdmin = FirebaseAdminApp.initializeApp(
-    'care360-26413',
-    Credential.fromServiceAccount(
-      File('service-account.json'),
+    projectId!,
+    Credential.fromServiceAccountParams(
+      clientId: clientId!,
+      email: cientEmail!,
+      privateKey: privateKey!,
     ),
   );
 
@@ -30,9 +39,9 @@ Future<HttpServer> run(Handler handler, InternetAddress ip, int port) async {
   // Start the server
   final server = await serve(handler, ip, port);
 
-  print(
+  /*print(
     '\n🏁 🏁 🏁 Server is running on ${server.address}:${server.port}...',
-  );
+  );*/
 
   // Listen for server shutdown and close Firebase Admin
   ProcessSignal.sigint.watch().listen((_) async {
